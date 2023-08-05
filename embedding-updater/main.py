21c4482823bcf01308ca3ab2aae901659445da48
Kaenova Mahendra Auditama <61568092+kaenova@users.noreply.github.com>
2023-08-05 14:30:30 +0700

    Update main.py

--- embedding-updater/main.py
+++ embedding-updater/main.py
@@ -1,4 +1,5 @@
 # NOTE: If you wnat to develop this, make sure you have dist.zip inside embedding-updater folder
+# Download the dist.zip through here: https://drive.google.com/file/d/13Fnm3ingRAmDnhu2P8p8wROBfhbD7QwL/view?usp=sharing
 
 import os
 import pathlib
@@ -15,7 +16,7 @@ with zipfile.ZipFile(zip_file,"r") as zip_ref:
 # Now image is saved on the same directory of this script with a folder 'dist'
 print(os.listdir(icons_folder))
 # output: ['vsc', 'rx', 'tfi', 'ri', 'lu', 'di', 'ti', 'fc', 'im', 'si', 'fa', 'lia', 'hi', 'hi2', 'bs', 'gi', 'sl', 'bi', 'fi', 'cg', 'wi', 'tb', 'io', 'ci', 'gr', 'pi', 'io5', 'ai', 'md', 'go', 'fa6']
-# The output abouve are folders of icon group. Inside that folders are jpeg of an icon with a code in the filename
+# The output above are folders of icon group. Inside that folders are jpeg of an icon with a code in the filename
 
 # TODO: Do smart image update
 # Smart update (don't update available icon, only update icon that isn't in the database)
